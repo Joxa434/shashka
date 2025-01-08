2f57c041a1606f2fc1fbe9b98ec324bb07b339ff
Javohir Imomaliyev <tube.tv.uchun@gmail.com>
2025-01-08 15:37:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,3 @@ def pawn(x1, y1, x2, y2):
 def miniKing(x1, y1, x2, y2, is_damka=False):
     if is_damka:
         return abs(x2 - x1) == abs(y2 - y1)
-    else:  
-        return y2 - y1 == 1 and abs(x2 - x1) == 1
